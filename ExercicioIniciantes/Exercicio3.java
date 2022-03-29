73953e329805a4c169ae11ce67847ce018108895
RhSantos <rhuan.santos.araujo@gmail.com>
2022-03-29 14:37:43 -0300

    Implementação Exercicio3

---
+++ ExercicioIniciantes/Exercicio3.java
@@ -0,0 +1,19 @@
+package ExercicioIniciantes;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        int varA = sc1.nextInt();
+        int varB = sc1.nextInt();
+        int varC = sc1.nextInt();
+        int varD = sc1.nextInt();
+        sc1.close();
+        System.out.println
+        ("DIFERENCA = "+(varA * varB - varC * varD));
+
+    }
+
+}
