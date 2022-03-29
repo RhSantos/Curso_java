9f5a7d5fd887239e98993f724a552e3e107bcde8
RhSantos <rhuan.santos.araujo@gmail.com>
2022-03-29 14:21:57 -0300

    Implementação do Exercicio1 e Refatoração dos Nomes do Pacote e Classe

---
+++ ExercicioIniciantes/Exercicio1.java
@@ -0,0 +1,15 @@
+package ExercicioIniciantes;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);    
+        int x = sc1.nextInt();
+        int y = sc1.nextInt();
+        System.out.println("SOMA = "+(x+y));    
+    }
+
+    
+}
