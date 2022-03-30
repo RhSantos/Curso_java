be6389f2a876c6b6ad991aa84b9ed056793b8fae
RhSantos <rhuan.santos.araujo@gmail.com>
2022-03-30 14:29:54 -0300

    Modificação ExercicioFixacao1

--- JavaPOO/ExercicioFixacao1/Main.java
+++ JavaPOO/ExercicioFixacao1/Main.java
@@ -2,4 +2,14 @@ package JavaPOO.ExercicioFixacao1;
 
 public class Main {
     
+    public static void main(String[] args) {
+        
+        Rectangle r1 = new Rectangle(3.00, 4.00);
+
+        System.out.println("AREA = "+r1.Area());
+        System.out.println("PERIMETRO = "+r1.Perimeter());
+        System.out.println("DIAGONAL = "+r1.Diagonal());
+
+    }
+
 }
--- JavaPOO/ExercicioFixacao1/Rectangle.java
+++ JavaPOO/ExercicioFixacao1/Rectangle.java
@@ -1,5 +1,49 @@
 package JavaPOO.ExercicioFixacao1;
 
+import java.lang.Math;
+
 public class Rectangle {
+    private double width;
+    private double height;
+
     
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+    public double getWidth() {
+        return width;
+    }
+
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+
+    public double getHeight() {
+        return height;
+    }
+
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double Area(){
+        double area = width * height;
+        return area;
+    }
+
+    public double Perimeter(){
+        double perimeter = (2* width) + (2 *height);
+        return perimeter;
+    }
+
+    public double Diagonal(){
+        double diagonal = Math.sqrt(Math.pow(width,2) + Math.pow(height,2));
+        return diagonal;
+    }
 }
