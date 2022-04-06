0c5aab6e0a3f6d06bf60f77e059ceef0aa7b1a87
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-06 15:41:28 -0300

    Mais Modificações

--- ExercicioFixacaoLista/Main.java
+++ ExercicioFixacaoLista/Main.java
@@ -34,22 +34,27 @@ public class Main {
             Funcionario funcionario = new Funcionario(id, nome, salario);
             funcionarios.add(funcionario);
         }
+    
+        System.out.println("Funcionários:");
 
         for (Funcionario funcionario : funcionarios) {
             System.out.println(funcionario);
         }
 
-        System.out.println("Digite o ID do Funcionário que você deseja aumentar o salário!");
+        System.out.println("\nDigite o ID do Funcionário que você deseja aumentar o salário!\n");
         int idSalario = sc.nextInt();
-        System.out.println();
         Integer posicao = temId(funcionarios,idSalario);
         if(temId(funcionarios,idSalario) != null){
+            System.out.println("\nDigite a porcentagem que quer aumentar!\n");
             double aumento = sc.nextDouble();
             funcionarios.get(posicao).aumentoSalario(aumento);
+            System.out.println("Lista de Funcionários Atualizada!\n");
+            for (Funcionario funcionario : funcionarios) {
+                System.out.println(funcionario);
+            }
         }
         else{
-            System.out.println("ID não Encontrado!");
-            System.out.println();
+            System.out.println("\nID não Encontrado!\n");
         }
         sc.close();
     }
