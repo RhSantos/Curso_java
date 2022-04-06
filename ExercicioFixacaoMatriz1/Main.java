0870a06f88f70aef57c4efd450732cc9b17f327a
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-06 17:47:26 -0300

    Última Modificação

--- ExercicioFixacaoMatriz1/Main.java
+++ ExercicioFixacaoMatriz1/Main.java
@@ -9,26 +9,26 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite o tamanho da Matriz!\n");
 		int n = sc.nextInt();
-		int[][] mat = new int[n][n];
+		int[][] matriz = new int[n][n];
 		
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
+		for (int i=0; i<matriz.length; i++) {
+			for (int j=0; j<matriz[i].length; j++) {
 				System.out.println("\nDigite um valor para acrescentar na Matriz!");
-				mat[i][j] = sc.nextInt();
+				matriz[i][j] = sc.nextInt();
 			}
 		}
 		
 		System.out.println("\nA Diagonal Principal é:");
-		for (int i=0; i<mat.length; i++) {
-			System.out.print(mat[i][i] + " ");
+		for (int i=0; i<matriz.length; i++) {
+			System.out.print(matriz[i][i] + " ");
 		}
 		System.out.println();
 		
 		int numerosNegativos = 0;
 
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
-				if (mat[i][j] < 0) {
+		for (int i=0; i<matriz.length; i++) {
+			for (int j=0; j<matriz[i].length; j++) {
+				if (matriz[i][j] < 0) {
 					numerosNegativos++;
 				}
 			}
