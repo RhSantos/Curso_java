f6c85b43249aa3757e5f01c9c87dcd1a16ea5a42
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-06 17:46:23 -0300

    Modificações

--- ExercicioFixacaoMatriz1/Main.java
+++ ExercicioFixacaoMatriz1/Main.java
@@ -7,31 +7,33 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("Digite o tamanho da Matriz!\n");
 		int n = sc.nextInt();
 		int[][] mat = new int[n][n];
 		
 		for (int i=0; i<mat.length; i++) {
 			for (int j=0; j<mat[i].length; j++) {
+				System.out.println("\nDigite um valor para acrescentar na Matriz!");
 				mat[i][j] = sc.nextInt();
 			}
 		}
 		
-		System.out.println("Main diagonal:");
+		System.out.println("\nA Diagonal Principal é:");
 		for (int i=0; i<mat.length; i++) {
 			System.out.print(mat[i][i] + " ");
 		}
 		System.out.println();
 		
-		int count = 0;
+		int numerosNegativos = 0;
+
 		for (int i=0; i<mat.length; i++) {
 			for (int j=0; j<mat[i].length; j++) {
 				if (mat[i][j] < 0) {
-					count++;
+					numerosNegativos++;
 				}
 			}
 		}
-		System.out.println("Negative numbers = " + count);
+		System.out.println("\nNúmeros Negativos = " + numerosNegativos);
 		
 		
 		sc.close();
