00eebf0c6387c9ac2c2d74ad652a23b9d7c8b428
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-07 17:10:22 -0300

    Tornando mais simples as condições e Valores Matriz digitados pelo usuário

--- ExercicioFixacaoMatrizes/Main.java
+++ ExercicioFixacaoMatrizes/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = (int)(1+Math.random()*49);
+                matriz[i][j] = sc.nextInt();
             }
         }
 
@@ -32,69 +32,19 @@ public class Main {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 if(matriz[i][j] == x) {
-                    if(i == 0 && j == 0){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Baixo: "+matriz[i+1][j]);
-                        System.out.println("Direita: "+matriz[i][j+1]);
-                        System.out.println();
-                    }
-                    else if (i == matriz.length-1 && j == matriz[i].length-1){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Cima: "+matriz[i-1][j]);
-                        System.out.println("Esquerda: "+matriz[i][j-1]);
-                        System.out.println();
-                    }
-                    else if (i == matriz.length-1 && j == 0){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Cima: "+matriz[i-1][j]);
-                        System.out.println("Direita: "+matriz[i][j+1]);
-                        System.out.println();
-                    }
-                    else if (i == 0 && j == matriz[i].length-1){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Baixo: "+matriz[i+1][j]);
-                        System.out.println("Esquerda: "+matriz[i][j-1]);
-                        System.out.println();
-                    }
-                    else if(i == 0 && j > 0 && j < matriz[i].length-1){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Baixo: "+matriz[i+1][j]);
-                        System.out.println("Esquerda: "+matriz[i][j-1]);
-                        System.out.println("Direita: "+matriz[i][j+1]);
-                        System.out.println();
-                    }
-                    else if(j == 0 && i > 0 && i < matriz.length-1){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Cima: "+matriz[i-1][j]);
-                        System.out.println("Baixo: "+matriz[i+1][j]);
-                        System.out.println("Direita: "+matriz[i][j+1]);
-                        System.out.println();
-                    }
-
-                    else if(i == matriz.length-1 && j > 0 && j < matriz[i].length-1){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Cima: "+matriz[i-1][j]);
-                        System.out.println("Esquerda: "+matriz[i][j-1]);
-                        System.out.println("Direita: "+matriz[i][j+1]);
-                        System.out.println();
-                    }
-
-                    else if(j == matriz.length-1 && i > 0 && i < matriz[i].length-1){
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Cima: "+matriz[i-1][j]);
-                        System.out.println("Baixo: "+matriz[i+1][j]);
-                        System.out.println("Esquerda: "+matriz[i][j-1]);
-                        System.out.println();
-                    }
-
-                    else{
-                        System.out.println("Posição "+i+","+j);
-                        System.out.println("Cima: "+matriz[i-1][j]);
-                        System.out.println("Baixo: "+matriz[i+1][j]);
-                        System.out.println("Esquerda: "+matriz[i][j-1]);
-                        System.out.println("Direita: "+matriz[i][j+1]);
-                        System.out.println();
-                    }
+                    System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Esquerda: " + matriz[i][j-1]);
+					}
+					if (i > 0) {
+						System.out.println("Cima: " + matriz[i-1][j]);
+					}
+					if (j < matriz[i].length-1) {
+						System.out.println("Direita: " + matriz[i][j+1]);
+					}
+					if (i < matriz.length-1) {
+						System.out.println("Baixo: " + matriz[i+1][j]);
+					}
                 }
             }
         }
