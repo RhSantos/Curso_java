963dcdeb4e2b7351bc13b60a09cb91dd385c661a
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-11 14:05:38 -0300

    Remoção de imports inuteis

--- secao13/ExercicioFixacao/application/Program.java
+++ secao13/ExercicioFixacao/application/Program.java
@@ -2,8 +2,6 @@ package secao13.ExercicioFixacao.application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
