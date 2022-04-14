060f76c91e75a4434d10692f20bd21ea106b134a
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-14 14:52:49 -0300

    ChessException e ChessPosition - Alterando posição tabuleiro para posições do Xadrez

--- secao16/ProjetoXadrez/chess/ChessMatch.java
+++ secao16/ProjetoXadrez/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package secao16.ProjetoXadrez.chess;
 
 import secao16.ProjetoXadrez.boardgame.Board;
-import secao16.ProjetoXadrez.boardgame.Position;
 import secao16.ProjetoXadrez.chess_pieces.*;
 
 public class ChessMatch {
@@ -23,12 +22,16 @@ public class ChessMatch {
         return mat;
     }
 
+    public void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column,row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board,Color.WHITE), new Position(0, 0));
-        board.placePiece(new Rook(board,Color.WHITE), new Position(0, 7));
-        board.placePiece(new Rook(board,Color.BLACK), new Position(7, 0));
-        board.placePiece(new Rook(board,Color.BLACK), new Position(7, 7));
-        board.placePiece(new King(board,Color.WHITE), new Position(0, 4));
-        board.placePiece(new King(board,Color.BLACK), new Position(7, 4));
+        placeNewPiece('a',1,new Rook(board,Color.WHITE));
+        placeNewPiece('h',1,new Rook(board,Color.WHITE));
+        placeNewPiece('a',8,new Rook(board,Color.BLACK));
+        placeNewPiece('h',8,new Rook(board,Color.BLACK));
+        placeNewPiece('e',1,new King(board,Color.WHITE));
+        placeNewPiece('e',8,new King(board,Color.BLACK));
     }
 }
