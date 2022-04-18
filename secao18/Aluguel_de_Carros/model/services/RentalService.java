70960ac8089830296bddf404800bdd1c2c6bed07
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-18 15:34:24 -0300

    Utilizando Interface

--- secao18/Aluguel_de_Carros/model/services/RentalService.java
+++ secao18/Aluguel_de_Carros/model/services/RentalService.java
@@ -6,9 +6,9 @@ import secao18.Aluguel_de_Carros.model.entities.Invoice;
 public class RentalService {
     private Double pricePerHour;
     private Double pricePerDay;
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
         this.pricePerDay = pricePerDay;
         this.taxService = taxService;
@@ -30,11 +30,11 @@ public class RentalService {
         this.pricePerDay = pricePerDay;
     }
 
-    public BrazilTaxService getTaxService() {
+    public TaxService getTaxService() {
         return taxService;
     }
 
-    public void setTaxService(BrazilTaxService taxService) {
+    public void setTaxService(TaxService taxService) {
         this.taxService = taxService;
     }
 
---
+++ secao18/Aluguel_de_Carros/model/services/TaxService.java
@@ -0,0 +1,5 @@
+package secao18.Aluguel_de_Carros.model.services;
+
+public interface TaxService {
+    double tax(double amount);
+}
