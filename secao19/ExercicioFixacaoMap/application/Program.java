d9a0776a50dbc0f8c20f01598883d2304152258a
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-19 17:29:37 -0300

    Implementação

---
+++ secao19/ExercicioFixacaoMap/application/Program.java
@@ -0,0 +1,32 @@
+package secao19.ExercicioFixacaoMap.application;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Program {
+    public static void main(String[] args) {
+        Map<String,Integer> eleicao = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("secao19/ExercicioFixacaoMap/votos.csv"))){
+            String line;
+            while((line = br.readLine()) != null){
+                line = line.trim();
+                String[] fields = line.split(",");
+                String nome = fields[0];
+                Integer votos = Integer.parseInt(fields[1]);
+                eleicao.put(nome, votos);
+            }
+            System.out.println("--ELEIÇÃO 2022--\n");
+            for (String key : eleicao.keySet()) {
+                System.out.print("Nome do Candidato: "+key+" Votos: "+eleicao.get(key)+"\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not Found");
+        } catch(IOException e){
+            System.out.println("Cannot read the File");
+        }
+    }
+}
