411e14e05657ee80bc5eb4618dcd8f900b702bc1
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-19 16:23:46 -0300

    Implementação

---
+++ secao19/ExercicioResolvidoSet/application/Program.java
@@ -0,0 +1,33 @@
+package secao19.ExercicioResolvidoSet.application;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Set<User> set = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("in.txt"))){
+            String line;
+            
+            while((line = br.readLine()) != null){
+                line = line.trim();
+                String[] fields = line.split(" ");
+                Date date = Date.from(Instant.parse(fields[1]));
+                set.add(new User(fields[0], date));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found"); 
+        } catch (IOException e) {
+            System.out.println("Error reading the file");
+        }
+
+        System.out.println("Total Users: " +set.size());
+    }
+}
